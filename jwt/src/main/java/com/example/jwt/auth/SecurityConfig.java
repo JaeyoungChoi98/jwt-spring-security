b30109a1b9645facdaeca53225ddffa8da8e0171
220SRS <jesikrjy@gmail.com>
2023-06-19 16:55:08 +0900

    Feat : jwt 구현

--- jwt/src/main/java/com/example/jwt/auth/SecurityConfig.java
+++ jwt/src/main/java/com/example/jwt/auth/SecurityConfig.java
@@ -1,14 +1,28 @@
 package com.example.jwt.auth;
 
+import com.example.jwt.auth.jwtToken.JwtAuthenticationFilter;
+import com.example.jwt.auth.jwtToken.JwtTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
+    private final JwtTokenizer jwtTokenizer;
+
+    public SecurityConfig(JwtTokenizer jwtTokenizer) {
+        this.jwtTokenizer = jwtTokenizer;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -16,12 +30,42 @@ public class SecurityConfig {
                 .headers().frameOptions().sameOrigin()
                 .and()
                 .csrf().disable()
-                .cors(withDefaults())
+                .cors().configurationSource(corsConfigurationSource())
+                .and()
                 .formLogin().disable()
                 .httpBasic().disable()
+                .apply(new CustomFilterConfigurer())
+                .and()
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().permitAll());
         return http.build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST","PATCH","DELETE"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+
+    public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
+        @Override
+        public void configure(HttpSecurity builder) throws Exception {
+            AuthenticationManager authenticationManager = builder.getSharedObject(AuthenticationManager.class);
+
+            JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer);
+            jwtAuthenticationFilter.setFilterProcessesUrl("/login");
+
+            builder.addFilter(jwtAuthenticationFilter);
+        }
+    }
 }
--- jwt/src/main/java/com/example/jwt/member/MemberService.java
+++ jwt/src/main/java/com/example/jwt/member/MemberService.java
@@ -2,18 +2,36 @@ package com.example.jwt.member;
 
 import com.example.jwt.member.dto.MemberResponseDto;
 import com.example.jwt.member.dto.SignUpDto;
-import lombok.RequiredArgsConstructor;
+
+import com.example.jwt.utils.CustomAuthorityUtils;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-@RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    //비번 생성기
+    private final PasswordEncoder passwordEncoder;
+
+    //역할 부여
+    private final CustomAuthorityUtils customAuthorityUtils;
+
+    public MemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder, CustomAuthorityUtils customAuthorityUtils) {
+        this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.customAuthorityUtils = customAuthorityUtils;
+    }
+
     public MemberResponseDto saveMember(SignUpDto sign) {
 
-        Member member = Member.of(sign);
+        String encryptedPassword = passwordEncoder.encode(sign.getPassword());
+        List<String> roles = customAuthorityUtils.createRole(sign.getEmail());
+
+        Member member = Member.of(sign, encryptedPassword, roles);
 
         memberRepository.save(member);
 
